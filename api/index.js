4f2e08bb8c163af475c88a0a09d29037a707b179
KavithRashintha <wgkrashintha@gmail.com>
2024-08-28 17:31:58 +0530

    CORS Error is fixed

--- api/index.js
+++ api/index.js
@@ -9,11 +9,18 @@ const UserRoute=require('./routes/userRoute');
 const OrderRoute=require('./routes/orderRoute');
 const ProductRoute=require('./routes/productRoute');
 const CartRoute=require('./routes/cartRoute');
+const cors = require("cors");
 
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: 'http://localhost:4200',  // Replace with the actual origin of your front-end
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // List of allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // List of allowed headers
+}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/BigDealsLK').then(()=>{
     app.listen(port , ()=>{
